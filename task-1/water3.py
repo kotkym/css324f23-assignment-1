97348aa116e9e79c87bc3ffefad5a6e453c31245
kotkym <144599481+kotkym@users.noreply.github.com>
2023-09-12 19:14:57 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -10,35 +10,35 @@ def successors(s):
   t = 5-y
   if a > 0 and x > 0:
     if x > a:
-      yield ((x-a, 5, z), t)
+      yield ((x-t, 5, z), t)
     else:
       yield ((0, y+x, z), x)
 
-  a = 8-x
-  if a > 0 and y > 0:
-    if y > a:
-      yield ((8, y-a, z), a)
+  t = 8-x
+  if t > 0 and y > 0:
+    if y > t:
+      yield ((8, y-t, z), t)
     else:
       yield ((x+y, 0, z), y)
 
-  a = 3-z
-  if a > 0 and y > 0:
-    if y > a:
-      yield ((x, y-t, 3), a)
+  t = 3-z
+  if t > 0 and y > 0:
+    if y > t:
+      yield ((x, y-t, 3), t)
     else:
       yield ((x, 0, z+y), y)
 
-  a = 8-x
+  t = 8-x
   if a > 0 and z > 0:
-    if z > a:
-      yield((8, y, z-a), a)
+    if z > t:
+      yield((8, y, z-t), t)
     else:
       yield((x+z, y, 0), z)
       
-  a = 5-y
-  if a > 0 and z > 0:
-    if z > a:
-      yield((x, 5, z-a), a)
+  t = 5-y
+  if t > 0 and z > 0:
+    if z > t:
+      yield((x, 5, z-t), t)
     else:
       yield((x, y+z, 0), z)
 
