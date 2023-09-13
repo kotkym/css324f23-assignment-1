c2086dfe60f59a405ee49153131cb60c0b474fa0
ATundon <144599495+ATundon@users.noreply.github.com>
2023-09-13 11:08:07 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -2,13 +2,13 @@ def initial_state():
     return (8, 0, 0)
 
 def is_goal(s):
-    return (s[0] == 4 abd s[1] == 4)
+    return (s[0] == 4 and s[1] == 4)
 
 def successors(s):
   x, y, z = s
   
   t = 5-y
-  if a > 0 and x > 0:
+  if t > 0 and x > 0:
     if x > a:
       yield ((x-t, 5, z), t)
     else:
